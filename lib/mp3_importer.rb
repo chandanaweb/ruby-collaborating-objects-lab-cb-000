24e42ac6c3223553c525d9e3c3715c33a88134d8
chandana M <chandu11288@gmail.com>
2018-04-26 01:16:35 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,13 @@
+class MP3Importer
+  attr_accessor :path
+  def initialize(path)
+    @path = path
+    @files = []
+  end
+  def files
+    @files ||= Dir.glob("#{@path}/*.mp3").collect{ |f| f.gsub("#{@path}/", "") }
+  end
+  def import(list_of_filenames)
+      list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+    end
+end
